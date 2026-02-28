6ce498fbf4b45bf6c5cec4cb27359209ded66c37
Fantabrand Properties <hemmythewunderkind@gmail.com>
2026-02-28 16:18:43 +0100

    Fixed PropertyGrid data rendering

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import HeroSlider from "@/components/HeroSlider"
-import PropertyGrid from "@/components/PropertyGrid"
+import HeroSlider from "@/components/HeroSlider";
+import PropertyGrid from "@/components/PropertyGrid";
+import properties from "@/data/properties";
 
 export default function Home() {
   return (
     <>
       <HeroSlider />
-      <PropertyGrid />
+
+      <PropertyGrid
+        properties={properties}
+        title="Featured Properties"
+        subtitle="Exclusive luxury listings curated for you"
+      />
     </>
-  )
+  );
 }
