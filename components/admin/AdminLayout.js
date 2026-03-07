8e7251e0210ea7ba4b659628607bb0bb6283874d
Fantabrand Properties <hemmythewunderkind@gmail.com>
2026-03-07 13:04:31 +0100

    Mobile admin sidebar toggle upgrade

--- components/admin/AdminLayout.js
+++ components/admin/AdminLayout.js
@@ -1,17 +1,47 @@
+import { useState } from "react";
 import AdminSidebar from "./AdminSidebar";
 import AdminProtectedRoute from "./AdminProtectedRoute";
 import styles from "../../styles/AdminLayout.module.css";
 
 export default function AdminLayout({ children }) {
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
 
     <AdminProtectedRoute>
 
+      {/* Mobile Menu Button */}
+      <button
+        className={styles.menuButton}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+
+      {/* Overlay */}
+      {menuOpen && (
+        <div
+          className={styles.overlay}
+          onClick={closeMenu}
+        />
+      )}
+
       <div className={styles.layout}>
 
-        <AdminSidebar />
+        {/* Sidebar */}
+        <div
+          className={`${styles.sidebar} ${menuOpen ? styles.sidebarOpen : ""}`}
+          onClick={closeMenu}
+        >
+          <AdminSidebar />
+        </div>
 
+        {/* Main Content */}
         <div className={styles.main}>
           {children}
         </div>
