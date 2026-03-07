deba46b41cf9e62f1f10028679b01ae04d506e47
Fantabrand Properties <hemmythewunderkind@gmail.com>
2026-03-07 17:27:00 +0100

    Fix missing Link import on news page

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabase/client";
+import Link from "next/link";
 export default function AdminNews(){
 
   const [news,setNews] = useState([]);
